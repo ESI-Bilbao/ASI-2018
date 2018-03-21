f9a0ae0be447f753bda12296a746b05b36394589
Armando Ferro <armando.ferro@ehu.es>
2018-03-21 09:24:52 +0100

    Preparada estructura para práctica-3

---
+++ src/practica-3/client.h
@@ -0,0 +1,23 @@
+
+
+#define PRACTICA 2
+
+#define VERSION "0.0"
+
+#define FIFO_PATH "/tmp/"	// Para trabajr en directorio temporal
+#define FIFO_NAME_WR "fifo_monitor_1"
+#define FIFO_NAME_RD "fifo_monitor_2"
+
+#define TMP_CHECK 5	// Tiempo para testear si existe fichero
+#define N_CHECK 3	// Número de intentos de fichero
+#define TMP_RD_FIFO 20		// Temporizador para leer
+#define TMP_RD_QUEUE TMP_RD_FIFO
+
+#define TMP_INTER_EXER 3
+
+#define MOD_RND_MSG 15L
+#define MIN_RND_MSG 2
+
+#define DNI 1122334455L
+
+#define CLAVE DNI
